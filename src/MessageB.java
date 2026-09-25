8f9b0469be5414a70d2caead81d8a2cab7b6a881
MOANKOANG Chloe <moankoangchloe@gmail.com>
2026-09-25 13:41:19 +0200

    Ajout d'une deuxième ligne au message de Astrid

--- src/MessageB.java
+++ src/MessageB.java
@@ -1,5 +1,6 @@
 public class MessageB {
     public static void main(String[] args) {
-        System.out.println("Message du membre B");
+        System.out.println("Message de Astrid");
+        System.out.println("Deuxième ligne de Astrid");
     }
 }
